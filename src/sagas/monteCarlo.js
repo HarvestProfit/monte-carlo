d721dc3925e05f487fc128b4f267c20d40f7eb43
Jaryd Krishnan <jaryd@harvestprofit.com>
2018-02-21 03:08:28 -0600

    Dramatic performance enhancement by grouping iterations

--- src/sagas/monteCarlo.js
+++ src/sagas/monteCarlo.js
@@ -22,10 +22,24 @@ import {
  * @param {number} price The price to start at
  * @param {number} dailyVolatility The potential daily volatility. NOT annual. DAILY.
  */
-function* runSingleIteration(steps, price, dailyVolatility, iteration) {
-  const lastPrice = yield call(MonteCarlo.singleIteration, steps, price, dailyVolatility);
-  yield put({ type: ADD_ITERATIONS, payload: [lastPrice] });
-  yield put({ type: PROGRESS_UPDATE, payload: iteration + 1 });
+function runSingleIteration(steps, price, dailyVolatility) {
+  const lastPrice = MonteCarlo.singleIteration(steps, price, dailyVolatility);
+  return lastPrice;
+}
+
+/**
+ * Runs a single iteration
+ * @param {number} iterations The iterations to loop through
+ * @param {number} steps The number of steps in this iteration
+ * @param {number} price The price to start at
+ * @param {number} dailyVolatility The potential daily volatility. NOT annual. DAILY.
+ */
+function loopThroughIterations(iterations, steps, price, volatility) {
+  const results = [];
+  for (let iteration = 0; iteration < iterations; iteration += 1) {
+    results.push(runSingleIteration(steps, price, volatility, iteration));
+  }
+  return results;
 }
 
 /**
@@ -39,8 +53,28 @@ function* runMonteCarloSimulation(iterations, price, volatility, steps) {
   try {
     yield put({ type: PROGRESS_CLEAR });
     yield put({ type: CLEAR_ITERATIONS });
-    for (let iteration = 0; iteration < iterations; iteration += 1) {
-      yield call(runSingleIteration, steps, price, volatility, iteration);
+    if (iterations > 5000) {
+      const iterationsToRun = yield Math.floor(iterations / 5000);
+      const remainingIterations = yield iterations % 5000;
+      // Loop through iterations in groups of 5000
+      for (let loop = 0; loop < iterationsToRun; loop += 1) {
+        const results = yield call(loopThroughIterations, 5000, steps, price, volatility);
+        yield put({ type: ADD_ITERATIONS, payload: results });
+        yield put({ type: PROGRESS_UPDATE, payload: 5000 * loop });
+      }
+      // Get the remaining iterations
+      const finalResults = yield call(
+        loopThroughIterations,
+        remainingIterations,
+        steps, price,
+        volatility,
+      );
+      yield put({ type: ADD_ITERATIONS, payload: finalResults });
+      yield put({ type: PROGRESS_UPDATE, payload: iterations });
+    } else {
+      const results = yield call(loopThroughIterations, iterations, price, volatility, steps);
+      yield put({ type: ADD_ITERATIONS, payload: results });
+      yield put({ type: PROGRESS_UPDATE, payload: iterations });
     }
     yield put({ type: END_MONTE_CARLO });
   } catch (error) {
